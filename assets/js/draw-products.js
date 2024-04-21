8e92ce12bf8a7074013fe0365acfd56ccecdc02b
bryanguevara <bryanguevara38@gmail.com>
2024-04-20 18:33:54 -0600

    SE TERMINO EL TRABAJO

--- assets/js/draw-products.js
+++ assets/js/draw-products.js
@@ -1,41 +1,89 @@
-let sections = ['featured', 'fast', 'best', 'trends'];
+let sections = ['featured', 'fast', 'best', 'Trends'];
 
 const getData = async (category) => {
+
+  let isInteger = Number.isInteger(category);
 
   return await fetch("assets/json/products.json")
     .then(res => res.json())
     .then((res) => {
+      var count = 0;
+      var count0 = 0;
+      var count1 = 0;
+      var count2 = 0;
+      var count3 = 0;
 
-      let filter = res.filter(function (res, i) {
+      // var fil = res.filter(res => res.category === 'featured')
+
+      let filter = res.filter(function (res) {
         // return res.categoria === 'Perecedero';
 
-        if (i <= 4 && category === 'fast') {
-          console.log(i)
-          return res.section === category;
+        if (category === 'all'){
+
+          if(res.section === sections[0]){
+            count0 += 1;
+            if (count0 <= 5){
+              return res.section === sections[0];
+            }
+          }
+
+          if(res.section === sections[1]){
+            count1 += 1;
+            if (count1 <= 5){
+              return res.section === sections[1];
+            }
+          }
+
+          if(res.section === sections[2]){
+            count2 += 1;
+            if (count2 <= 5){
+              return res.section === sections[2];
+            }
+          }
+
+          if(res.section === sections[3]){
+            count3 += 1;
+            console.log(count3)
+            if (count3 <= 5){
+              return res.section === sections[3];
+            }
+          }
+          
+        }else if (isInteger) {
+          return res.id === category;
+        } else{
+          if (res.section === category) {
+            count += 1;
+            if (count <= 5){
+              return res.section === category;
+            }
+          }
         }
-        // if (category === 'No perecedero') {
-        //   return res.categoria === category;
-        // }
-        // if (category === 'all') {
-        //   return res;
-        // }
       });
       return filter;
     });
 };
 
-console.log(getData('featured'))
-
 const drawProduct = async (category) => {
-  let container = document.getElementById('container-products');
-  let cards = '';
   var data = await getData(category);
+  var container0 = document.getElementById(sections[0]);
+  var container1 = document.getElementById(sections[1]);
+  var container2 = document.getElementById(sections[2]);
+  var container3 = document.getElementById(sections[3]);
+  let cards0 = '';
+  let cards1 = '';
+  let cards2 = '';
+  let cards3 = '';
+  let cards = '';
+
+  // items q  ue no van
+  // 56, 63, 45, 50
 
   data.forEach(e => {
     cards += `<div class="product-card">
     <div class="box-img">
       <figure>
-        <img src="assets/img/products/sh-1.jpg" alt="">
+        <img src="assets/img/products/${e.img_name}.png" alt="">
       </figure>
     </div>
     <div class="info-product">
@@ -45,13 +93,32 @@ const drawProduct = async (category) => {
       <p class="description">
         ${e.description}
       </p>
-      <button class="btn-add" onclick="insertProduct(this)" data-item='${e.item}>+ Add</button>
+      <button class="btn-add" onclick="insertProduct(this)" data-item='${e.id}'>+ Add</button>
 
     </div>
   </div>`;
+    if(e.section === sections[0]){
+      cards0 += cards;
+    }
+    if(e.section === sections[1]){
+      cards1 += cards;
+    }
+    if(e.section === sections[2]){
+      cards2 += cards;
+    }
+    if(e.section === sections[3]){
+      cards3 += cards;
+    }
+    cards = '';
   });
-  container.innerHTML = cards
+  container0.innerHTML = cards0;
+  container2.innerHTML = cards2;
+  container3.innerHTML = cards3;
+  // container.innerHTML = cards
 }
+
+console.log(drawProduct('all'))
+
 
 // CLICK BTN PERECEDEROS
 
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -9,63 +9,60 @@ var sale = {
         products_review: []
     },
     calculateInvoice: function () {
+        let total = document.getElementById('total-mount')
         // var subtotal_exempt = 0.00;
         var subtotal = 0.00;
         // var iva = $('input[name="iva"]').val();
         // var discount = $('input[name="discount"]').val();
         this.details.products.forEach(function (value, index, array) {
-            console.log(value)
+            let ttProduct = document.getElementById(value.id)
             value.index = index;
 
             value.cant = parseInt(value.cant);
-            value.subtotal = value.cant * parseFloat(value.precio);
+            value.subtotal = value.cant * parseFloat(value.price);
             subtotal += value.subtotal;
+            ttProduct.innerHTML = value.subtotal;
         });
+        this.details.total = subtotal;
 
-        // this.details.subtotal_exempt = subtotal_exempt;
-        this.details.subtotal = subtotal;
-        // this.details.discount = discount;
-
-        // this.details.iva = (this.details.subtotal - this.details.discount) * 0.15;
-        // this.details.total = ((this.details.subtotal + this.details.subtotal_exempt) - this.details.discount) + this.details.iva;
-
-        // $('input[name="subtotal"]').val(this.details.subtotal.toFixed(2));
-        // $('input[name="subtotal_exempt"]').val(this.details.subtotal_exempt.toFixed(2));
-        // $('input[name="ivacalc"]').val(this.details.iva.toFixed(2));
-        // $('input[name="total"]').val(this.details.total.toFixed(2));
+        total.innerHTML = subtotal;
     },
-    listProduct: function (e) {
+    listProduct: function () {
+        let cards = '';
         let container = document.getElementById('continer-shop-items');
-        const element = document.createElement("div");
-        element.setAttribute("class", "shop-item")
-
-        let card = `<div class="shop-img">
+        // const element = document.createElement("div");
+        // element.setAttribute("class", "shop-item")
+        this.details.products.forEach(e =>{
+            cards += `<div class="shop-item">
+            <div class="shop-img">
                 <figure>
-                    <img src="img/products/${e.img}.png" alt="">
+                    <img src="assets/img/products/${e.img_name}.png" alt="">
                 </figure>
             </div>
             <div class="shop-info">
-                <h3>${e.producto}</h3>
-                <span>${e.precio}</span>
-                <h6>Cacao</h6>
-                <p class="shop-p">${e.proveedor}</p>
+                <h6>${e.description}</h6>
+                <span>C$ ${e.price}</span>
+                <p class="shop-p">${e.category}</p>
                 <div class="container-btn-shop">
                     <div class="input-group input-group-sm mb-3">
                         <span class="input-group-append">
-                            <button type="button" class="btn btn-danger btn-flat">-</button>
+                                <button onclick="deleteItem(${e.id})" type="button" class="btn btn-danger btn-flat">
+                                    <i class="fa fa-trash"></i>
+                                </button>
                         </span>
-                        <input type="text" value="1" class="form-control rounded-0">
-                        <span class="input-group-append">
-                            <button type="button" class="btn btn-success btn-flat btn-menu-+">+</button>
-                        </span>
+                        <input onchange="btnShopPlus(this)" data-id="${e.id}" name="cant" type="number" min+"1" max="1000" step="1" value="1" class="form-control rounded-0">
                     </div>
-
+                    <hr>
+                    <span>Total producto: C$<span id="${e.id}">0.00</span></span>
+                    </div>
                 </div>
             </div>`;
+        });
 
-        element.innerHTML = card
-
-        container.appendChild(element)
+        // element.innerHTML = card
+        container.innerHTML = cards;
+        this.calculateInvoice();
+        // container.appendChild(element)
 
     }
 }
@@ -75,16 +72,46 @@ const insertProduct = async (e) => {
     let searchedItem = await getData(parseInt(e.dataset.item));
 
     // console.log(sale.details.products.map(value => value.Item))
-    console.log(sale.details)
+    // console.log(sale.details)
     // console.log(searchedItem)
 
     contador.innerHTML = parseInt(contador.textContent) + 1;
     searchedItem[0].cant = 1;
     searchedItem[0].subtotal = 0.00;
     sale.details.products.push(searchedItem[0]);
-    sale.calculateInvoice();
-    sale.listProduct(searchedItem[0]);
+    sale.listProduct();  
+    // sale.calculateInvoice();
 }
+
+const deleteItem = (id) =>{
+    let contador = document.getElementById('count-items');
+    contador.innerHTML = parseInt(contador.textContent) - 1;
+    let index = sale.details.products.findIndex(index => index.id == id);
+    sale.details.products.splice(index, 1);
+    sale.listProduct();
+}
+
+const btnShopPlus = (e) =>{
+    let id = e.getAttribute('data-id');
+    let index = sale.details.products.findIndex(index => index.id == id);
+    sale.details.products[index].cant = $(e).val();
+    sale.calculateInvoice()
+}
+
+$(function(){
+    $('#container-count').on('click', function(){
+        Swal.fire({
+            title: "Proceso de compra",
+            text: "Su pedido será entregado en un periodo de 1 hábil",
+            // icon: "question"
+          });
+        sale.details.products = [];
+        sale.listProduct();
+        let contador = document.getElementById('count-items');
+        contador.innerHTML = 0;
+    })
+})
+
 
 let menuShop = document.querySelector('#shop-menu'); // select the buttons on the menu panel
 // let links = document.querySelectorAll('a[class="navbar-link"]'); // Select all links from the panel menu
