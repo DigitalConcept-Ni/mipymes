da91d7fc5cf92c5f671c9c4912e65863fd3d2261
bryanguevara <bryanguevara38@gmail.com>
2024-05-16 21:19:28 -0600

    SE REALIZARON LOS CAMBIOS Y OBSERVACIONES POR EL CLIENTE, SE AGREGO UN SISTEMA DE LOGIN, SISTEMA DE COMPRA CON TARJETA

--- assets/js/draw-products.js
+++ assets/js/draw-products.js
@@ -7,7 +7,6 @@ const getData = async (category) => {
   return await fetch("assets/json/products.json")
     .then(res => res.json())
     .then((res) => {
-      var count = 0;
       var count0 = 0;
       var count1 = 0;
       var count2 = 0;
@@ -15,66 +14,73 @@ const getData = async (category) => {
 
       // var fil = res.filter(res => res.category === 'featured')
 
-      let filter = res.filter(function (res) {
-        // return res.categoria === 'Perecedero';
+      if (isInteger) {  
+        return fil = res.filter(res => res.id === category);
+      }
 
-        if (category === 'all'){
+      if (category !== 'all') {
+        return fil = res.filter(res => res.category === category);
+      } else {
+        let filter = res.filter(function (res) {
+          // return res.categoria === 'Perecedero';
+          if (category === 'all') {
 
-          if(res.section === sections[0]){
-            count0 += 1;
-            if (count0 <= 5){
-              return res.section === sections[0];
+            if (res.section === sections[0]) {
+              count0 += 1;
+              if (count0 <= 5) {
+                return res.section === sections[0];
+              }
             }
-          }
 
-          if(res.section === sections[1]){
-            count1 += 1;
-            if (count1 <= 5){
-              return res.section === sections[1];
+            if (res.section === sections[1]) {
+              count1 += 1;
+              if (count1 <= 5) {
+                return res.section === sections[1];
+              }
             }
-          }
 
-          if(res.section === sections[2]){
-            count2 += 1;
-            if (count2 <= 5){
-              return res.section === sections[2];
+            if (res.section === sections[2]) {
+              count2 += 1;
+              if (count2 <= 5) {
+                return res.section === sections[2];
+              }
             }
-          }
 
-          if(res.section === sections[3]){
-            count3 += 1;
-            if (count3 <= 5){
-              return res.section === sections[3];
+            if (res.section === sections[3]) {
+              count3 += 1;
+              if (count3 <= 5) {
+                return res.section === sections[3];
+              }
             }
+
           }
-          
-        }else if (isInteger) {
-          return res.id === category;
-        } else{
-          if (res.section === category) {
-            count += 1;
-            if (count <= 5){
-              return res.section === category;
-            }
-          }
-        }
-      });
-      return filter;
+        });
+        return filter;
+
+      }
     });
 };
 
 const drawProduct = async (category) => {
   var data = await getData(category);
-  var container0 = document.getElementById(sections[0]);
-  var container1 = document.getElementById(sections[1]);
-  var container2 = document.getElementById(sections[2]);
-  var container3 = document.getElementById(sections[3]);
-  let cards0 = '';
-  let cards1 = '';
-  let cards2 = '';
-  let cards3 = '';
-  let cards = '';
 
+  if (category === 'all') {
+    var container0 = document.getElementById(sections[0]);
+    var container1 = document.getElementById(sections[1]);
+    var container2 = document.getElementById(sections[2]);
+    var container3 = document.getElementById(sections[3]);
+    var cards0 = '';
+    var cards1 = '';
+    var cards2 = '';
+    var cards3 = '';
+    var cards = '';
+
+  } else {
+    var cards = '';
+    var containerCategory = document.getElementById('container-category-porducts')
+  }
+
+  // BLOQUE PARA DIBUJAR LAS CARDS DE LOS PRODUCOS
   // items q  ue no van
   // 56, 63, 45, 50
 
@@ -82,11 +88,11 @@ const drawProduct = async (category) => {
     cards += `<div class="product-card">
     <div class="box-img">
       <figure>
-        <img src="assets/img/products/${e.img_name}.png" alt="">
+        <img src="assets/img/products/iremove/${e.img_name}.png" alt="">
       </figure>
     </div>
     <div class="info-product">
-      <span class="price">
+      <span class="price">  
         C$ ${e.price}
       </span>
       <p class="description">
@@ -96,27 +102,56 @@ const drawProduct = async (category) => {
 
     </div>
   </div>`;
-    if(e.section === sections[0]){
-      cards0 += cards;
+    if (category === 'all') {
+
+      if (e.section === sections[0]) {
+        cards0 += cards;
+      }
+      if (e.section === sections[1]) {
+        cards1 += cards;
+      }
+      if (e.section === sections[2]) {
+        cards2 += cards;
+      }
+      if (e.section === sections[3]) {
+        cards3 += cards;
+      }
+      cards = '';
     }
-    if(e.section === sections[1]){
-      cards1 += cards;
-    }
-    if(e.section === sections[2]){
-      cards2 += cards;
-    }
-    if(e.section === sections[3]){
-      cards3 += cards;
-    }
-    cards = '';
+
+
   });
-  container0.innerHTML = cards0;
-  container2.innerHTML = cards2;
-  container3.innerHTML = cards3;
-  // container.innerHTML = cards
+  if (category === 'all') {
+
+    container0.innerHTML = cards0;
+    container1.innerHTML = cards1;
+    container2.innerHTML = cards2;
+    container3.innerHTML = cards3;
+  } else {
+    document.getElementById('title-category').innerText = `${category.charAt(0).toUpperCase() + category.toLowerCase().slice(1)}`;
+    containerCategory.style.display = 'grid'
+    containerCategory.innerHTML = cards
+
+  }
 }
 
-drawProduct('all')
+drawProduct('all');
+
+$('#catCapillary').on('click', function () {
+  drawProduct('CAPILAR');
+})
+$('#catMakeUp').on('click', function () {
+  drawProduct('MAQUILLAJE');
+})
+$('#catManicure').on('click', function () {
+  drawProduct('MANICURA');
+})
+$('#catPerfumery').on('click', function () {
+  drawProduct('PERFUMERIA');
+})
+$('#catPersonalCare').on('click', function () {
+  drawProduct('CUIDADO PERSONAL');
+})
 
 
 // CLICK BTN PERECEDEROS
--- assets/rename.py
+++ assets/rename.py
@@ -1,16 +1,31 @@
 import json
 import os
 
-with open('/Users/macbookpro/Documents/repo/mipymes/assets/json/products.json', 'r') as j:
-    myData = json.load(j)
+# with open('/Users/macbookpro/Documents/repo/mipymes/assets/json/products.json', 'r') as j:
+#     myData = json.load(j)
 
-ejemplo_dir = '/Users/macbookpro/Documents/repo/mipymes/assets/img/products/iremove'
+ejemplo_dir = "/Users/macbookpro/Documents/repo/mipymes/assets/img/products/iremove"
 content = os.listdir(ejemplo_dir)
 
-for i in myData:
-    for fichero in content:
-        if os.path.isfile(os.path.join(ejemplo_dir, fichero)) and fichero.endswith('.png'):
-            s = fichero.split('-')[1]
-            fit = os.path.join(ejemplo_dir, fichero)
-            if int(i['id']) == int(s):
-                os.rename(fit, os.path.join(ejemplo_dir, i['img_name']))
+i = []
+
+for fichero in content:
+    if os.path.isfile(os.path.join(ejemplo_dir, fichero)) and fichero.endswith(".png"):
+        s = fichero.split("-")[2].split('.')[0]
+        i.append(int(s))
+        
+        
+notListed = []        
+for x in range(1, 105):
+    if not x in i:
+         notListed.append(x)
+         
+# i.sort()
+print(notListed)
+# for i in myData:
+# for fichero in content:
+#     if os.path.isfile(os.path.join(ejemplo_dir, fichero)) and fichero.endswith('.png'):
+#         s = fichero.split('-')[1]
+#         fit = os.path.join(ejemplo_dir, fichero)
+#         if int(i['id']) == int(s):
+#             os.rename(fit, os.path.join(ejemplo_dir, i['img_name']))
